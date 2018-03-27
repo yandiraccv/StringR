3ad3c8ecd894167d026d3e46cfa8ae956b76405f
Yandira Cuvero <yandira.cuvero@gmail.com>
2018-03-27 02:34:25 -0500

    Update Ejercicio.R

--- Ejercicio.R
+++ Ejercicio.R
@@ -6,8 +6,13 @@ library(dplyr)
 #2. Carga de base de datos
 con <- "https://github.com/yandiraccv/StringR/raw/master/base.xlsx"
 download.file(con,"base.xlsx",mode="wb")
-base <- loadWorkbook(file="base.xlsx", create=F)
+base <- readWorksheetFromFile("base.xlsx", sheet=1)
 
-#3. REvisión de la base de datos
+#3. Revisión de la base de datos
 glimpse(base)
+head(base, n= 5)
+
+#4. Dimensión de la base de datos
+dim(base)
+names(base)
 
